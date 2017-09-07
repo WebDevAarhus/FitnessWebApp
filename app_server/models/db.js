babff9db4c55db28d1e444552f827192fec40263
Monika Ruszczyk <stencja@gmail.com>
2017-09-07 17:41:42 +0200

    nodemon won't cooperate, but node ./bin/www works

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 var dbURI = 'mongodb://localhost/workout';
+if(process.env.NODE_ENV === 'production'){
+    dbURI = process.env.MONGOLAB_URI;
+}
+
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function () {
